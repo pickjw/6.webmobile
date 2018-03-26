43eaadd905ae2be6461ebf77bec56f2222a69320
pickjw <moon45kjh@naver.com>
2018-03-26 16:55:54 +0900

    6.webmobile

---
+++ 3일차(다양한 컴포넌트 작성시 활용법2)/phone.sql
@@ -0,0 +1,10 @@
+ create table phone(
+ productnum number primary key,
+ model varchar2(30) not null,
+ company varchar2(20) not null,
+ price number(10) not null,
+ color varchar2(20) not null,
+ image varchar2(100) not null);
+
+
+
